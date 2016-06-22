1b8aff5ef053a9c23587adbab492d97bd0cacd7c
cjlaaa <cjlaaa@gmail.com>
2016-06-22 17:29:08 +0800

    UI添加TableView.

--- Classes/UI.cpp
+++ Classes/UI.cpp
@@ -34,8 +34,78 @@ bool UI::Init()
         ccbReader->release();
         addChild(ccbNode,enZOrderFront);
         
+        CCTableView* tableView = CCTableView::create(this,
+                                                     ccp(m_tableViewBg->getContentSize().width*m_tableViewBg->getScaleX(),
+                                                         m_tableViewBg->getContentSize().height*m_tableViewBg->getScaleY()));
+        tableView->setDirection(kCCScrollViewDirectionHorizontal);
+        tableView->setPosition(ccp(0,0));
+        tableView->setDelegate(this);
+        tableView->setVerticalFillOrder(kCCTableViewFillTopDown);
+        this->addChild(tableView,enZOrderFront);
+        tableView->reloadData();
+        
         return true;
     } while (false);
     CCLog("Function UI::Init Error!");
     return false;
+}
+
+bool UI::onAssignCCBMemberVariable(cocos2d::CCObject * pTarget, const char * pMemberVariableName, cocos2d::CCNode * pNode)
+{
+    CCB_MEMBERVARIABLEASSIGNER_GLUE(this, "tableViewBg", CCSprite*, m_tableViewBg);
+    
+    return true;
+};
+
+void UI::tableCellTouched(CCTableView* table, CCTableViewCell* cell)
+{
+    CCLog("tableCellTouched,%d",cell->getIdx());
+}
+
+CCSize UI::tableCellSizeForIndex(CCTableView *table, unsigned int idx)
+{
+    CCSprite *sprite = CCSprite::create("Icon-114.png");
+    CCSize size = sprite->getContentSize();
+    sprite->release();
+    return size;
+}
+
+void UI::tableViewSubBtnCallback(CCObject* pSender)
+{
+    CCTableViewCell *cell = (CCTableViewCell*)(((CCMenuItemImage*)pSender)->getParent()->getParent());
+    CCLog("tableViewSubBtnCallback,%d",cell->getIdx());
+}
+
+
+CCTableViewCell* UI::tableCellAtIndex(CCTableView *table, unsigned int idx)
+{
+    CCString *string = CCString::createWithFormat("%d", idx);
+    CCTableViewCell *cell = table->dequeueCell();
+    if (!cell) {
+        cell = new CCTableViewCell();
+        cell->autorelease();
+        CCSprite *sprite = CCSprite::create("Icon-114.png");
+        sprite->setAnchorPoint(CCPointZero);
+        sprite->setPosition(ccp(0, 0));
+        cell->addChild(sprite);
+        
+        CCLabelTTF *label = CCLabelTTF::create(string->getCString(), "Helvetica", 20.0);
+        label->setPosition(CCPointZero);
+        label->setAnchorPoint(CCPointZero);
+        label->setTag(123);
+        cell->addChild(label);
+    }
+    else
+    {
+        CCLabelTTF *label = (CCLabelTTF*)cell->getChildByTag(123);
+        label->setString(string->getCString());
+    }
+    
+    
+    return cell;
+}
+
+unsigned int UI::numberOfCellsInTableView(CCTableView *table)
+{
+    return 5;
 }
--- Classes/UI.h
+++ Classes/UI.h
@@ -15,19 +15,29 @@ class UI : public CCLayer
 , public cocos2d::extension::CCBSelectorResolver
 , public cocos2d::extension::CCBMemberVariableAssigner
 , public cocos2d::extension::CCNodeLoaderListener
+, public cocos2d::extension::CCTableViewDataSource
+, public cocos2d::extension::CCTableViewDelegate
 {
 public:
     static UI* CreateUI();
     
     virtual cocos2d::SEL_MenuHandler onResolveCCBCCMenuItemSelector(cocos2d::CCObject * pTarget, const char * pSelectorName){};
     virtual cocos2d::extension::SEL_CCControlHandler onResolveCCBCCControlSelector(cocos2d::CCObject * pTarget, const char * pSelectorName){};
-    virtual bool onAssignCCBMemberVariable(cocos2d::CCObject * pTarget, const char * pMemberVariableName, cocos2d::CCNode * pNode){};
+    virtual bool onAssignCCBMemberVariable(cocos2d::CCObject * pTarget, const char * pMemberVariableName, cocos2d::CCNode * pNode);
     virtual bool onAssignCCBCustomProperty(CCObject* pTarget, const char* pMemberVariableName, cocos2d::extension::CCBValue* pCCBValue){};
     virtual void onNodeLoaded(cocos2d::CCNode * pNode, cocos2d::extension::CCNodeLoader * pNodeLoader){};
     virtual SEL_CallFuncN onResolveCCBCCCallFuncSelector(CCObject * pTarget, const char* pSelectorName){};
     
+    virtual void scrollViewDidScroll(cocos2d::extension::CCScrollView* view){};
+    virtual void scrollViewDidZoom(cocos2d::extension::CCScrollView* view){};
+    virtual void tableCellTouched(cocos2d::extension::CCTableView* table, cocos2d::extension::CCTableViewCell* cell);
+    virtual cocos2d::CCSize tableCellSizeForIndex(cocos2d::extension::CCTableView *table, unsigned int idx);
+    virtual cocos2d::extension::CCTableViewCell* tableCellAtIndex(cocos2d::extension::CCTableView *table, unsigned int idx);
+    virtual unsigned int numberOfCellsInTableView(cocos2d::extension::CCTableView *table);
+    void tableViewSubBtnCallback(CCObject* pSender);
 protected:
     bool Init();
+    CCSprite* m_tableViewBg;
 };
 
 #endif /* UI_hpp */
